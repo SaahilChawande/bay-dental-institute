f32b81a1ad9031c571003b699ef9efa2acc10a8e
SaahilChawande <chawandesaahil600@gmail.com>
2019-01-26 13:09:50 +0530

    Fixed bug

--- bay-dental-institute.com/add-to-cart-handler.php
+++ bay-dental-institute.com/add-to-cart-handler.php
@@ -1,16 +1,8 @@
 <?php
 
+include "conn.php";
+
 session_start();
-
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="baydentalinstitute";
-$conn=mysqli_connect($servername,$username,$password,$dbname);
-
-if(!$conn)   {
-    die("Connection error: " . mysqli_connect_error());
-}
 
 // Create functions
 
